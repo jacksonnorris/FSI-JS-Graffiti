a57383333ba93bba99e10a6acf2a61a5d4fe807a
jacksonnorris <jacksonnorr@gmail.com>
2022-01-27 18:38:06 -0500

    init commit

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 
 // Select some elements...
+document.querySelector('#page-header').style.textAlign = "left";
+$(document).ready(function() {
+    $('.dog-image').attr('style', 'border-radius: 50px');
+    $('.footer').css('borderStyle','solid');
+});
